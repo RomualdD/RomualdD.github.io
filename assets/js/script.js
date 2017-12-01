66523304ecc70bf6225e3e492ef46a6e5e0ad3b6
RomualdD <romualdducrocq1@gmail.com>
2017-12-01 09:13:31 +0100

    Ajout touche number

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,39 @@ $(function(){
       case 88: //x
       window.location.href = "#realisation";
         break;
+        case 82: //r
+        window.location.href = "cvnumerique.html";
+          break;
+          case 96: //0
+          alert('buh');
+            break;
+            case 97: //1
+            alert('buh');
+              break;
+          case 98: //2
+            alert('buh');
+              break;
+          case 99: //3
+            alert('buh');
+              break;
+          case 100: //4
+            alert('buh');
+              break;
+          case 101: //5
+            alert('buh');
+              break;
+          case 102: //6
+            alert('buh');
+              break;
+          case 103: //7
+              alert('buh');
+                break;
+          case 104: //8
+              alert('buh');
+                break;
+          case 105: //9
+              alert('buh');
+                break;
     }
   })
 })
