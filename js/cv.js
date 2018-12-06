28380691d60e849c80917241df7d27e2231a72da
romualdducrocq1@gmail.com <romualdducrocq1@gmail.com>
2018-12-06 12:14:00 +0100

    Modification et ajout clown

--- js/cv.js
+++ js/cv.js
@@ -117,6 +117,7 @@ window.onload = function() {
 									}
 								}
                 // personnage fait par Farheit de deviantart / ftabah / josmiley
+								// image blacksmith réalisé par Creative Uncut
 		return false;
 	}
 
